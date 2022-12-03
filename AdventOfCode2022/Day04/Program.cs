d0a58113605c153526fd69900696bfac3dcc3d3b
joko-dev <jokodev@outlook.de>
2022-12-03 16:36:02 +0100

    Day04 - Preparation

---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2022.SharedKernel;
+
+namespace Day04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 04: "));
+            Console.WriteLine("Rucksack content: ");
+            PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false);
+        }
+    }
+}
