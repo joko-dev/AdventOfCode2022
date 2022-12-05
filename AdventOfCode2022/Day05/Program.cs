8cf37b82dcd55e055da11e21b4f9a4353aa8291f
joko-dev <jokodev@outlook.de>
2022-12-05 20:31:15 +0100

    Day05 - Init

---
+++ AdventOfCode2022/Day05/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2022.SharedKernel;
+
+namespace Day05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 05: Supply Stacks"));
+            Console.WriteLine("Stacks and rearrangement procedure: ");
+            PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false);
+
+            
+        }
+    }
+}
