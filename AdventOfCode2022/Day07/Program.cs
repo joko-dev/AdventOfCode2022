094ba8fdea2d40fc0f0a888143aff57b820a25f8
joko-dev <jokodev@outlook.de>
2022-12-06 20:43:47 +0100

    Day07 - Init

---
+++ AdventOfCode2022/Day07/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2022.SharedKernel;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 07: "));
+            Console.WriteLine(": ");
+            PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false); 
+        }
+    }
+}
