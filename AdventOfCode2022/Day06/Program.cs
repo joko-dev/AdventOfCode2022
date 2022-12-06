e1cfa84ed101582b08dac27f975cb27baf8b5828
joko-dev <jokodev@outlook.de>
2022-12-06 20:39:50 +0100

    Day06 - Part 1

--- AdventOfCode2022/Day06/Program.cs
+++ AdventOfCode2022/Day06/Program.cs
@@ -6,11 +6,24 @@ namespace Day06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 06: "));
-            Console.WriteLine(": ");
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 06: Tuning Trouble"));
+            Console.WriteLine("Datastream: ");
             PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false);
 
-            
+            Console.WriteLine("Charactercount before start-of-packet marker: {0}", GetCharacterCount(puzzleInput.Lines[0], 4));
+        }
+
+        private static int GetCharacterCount(string datastream, int markerLength)
+        {
+            for(int i = markerLength; i < datastream.Length; i++)
+            {
+
+                if (!datastream.Substring(i - markerLength, markerLength).GroupBy(c => c).Any(g => g.Count() > 1))               
+                {
+                    return i;
+                }
+            }
+            throw new ArgumentException(nameof(datastream));
         }
     }
 }
