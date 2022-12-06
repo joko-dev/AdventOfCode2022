6063642111eed7f4073526cb0801fb7b97c18e97
joko-dev <61630682+joko-dev@users.noreply.github.com>
2022-12-06 20:41:34 +0100

    Day06 (#5)
    * Day 06 - Init
    
    * Day 06 - Part 1
    
    * Day 06 - Part 2

---
+++ AdventOfCode2022/Day06/Program.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2022.SharedKernel;
+
+namespace Day06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 06: Tuning Trouble"));
+            Console.WriteLine("Datastream: ");
+            PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false);
+
+            Console.WriteLine("Charactercount before start-of-packet marker: {0}", GetCharacterCount(puzzleInput.Lines[0], 4));
+            Console.WriteLine("Charactercount before start-of-message marker: {0}", GetCharacterCount(puzzleInput.Lines[0], 14));
+        }
+
+        private static int GetCharacterCount(string datastream, int markerLength)
+        {
+            for(int i = markerLength; i < datastream.Length; i++)
+            {
+
+                if (!datastream.Substring(i - markerLength, markerLength).GroupBy(c => c).Any(g => g.Count() > 1))               
+                {
+                    return i;
+                }
+            }
+            throw new ArgumentException(nameof(datastream));
+        }
+    }
+}
