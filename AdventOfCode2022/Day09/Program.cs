1fc18d1f4c41756510eaea5cce2ee6274d4c7139
joko-dev <jokodev@outlook.de>
2022-12-10 20:10:37 +0100

    Daay09 - Part 2

--- AdventOfCode2022/Day09/Program.cs
+++ AdventOfCode2022/Day09/Program.cs
@@ -12,23 +12,64 @@ namespace Day09
             Console.WriteLine("series of motions: ");
             PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), false);
 
-            List<Coordinate> postionsVisited = MoveRope(puzzleInput);
+            
+            List<Coordinate> positionsVisited = MoveRope(puzzleInput, 2);
+            Console.WriteLine("Visited positions (2 knots): {0}", positionsVisited.Count);
 
-            Console.WriteLine("Visited positions`: {0}", postionsVisited.Count);
+            positionsVisited = MoveRope(puzzleInput, 10);
+            Console.WriteLine("Visited positions (10 knots): {0}", positionsVisited.Count);
+            PrintVisited(positionsVisited);
         }
 
-        private static List<Coordinate> MoveRope(PuzzleInput puzzleInput)
+        private static void PrintVisited(List<Coordinate> positionsVisited)
+        {
+            int minX = positionsVisited.Min(x => x.X);
+            int maxX = positionsVisited.Max(x => x.X);
+            int minY = positionsVisited.Min(y => y.Y);
+            int maxY = positionsVisited.Max(y => y.Y);
+
+            char[,] output = new char[maxX + Int32.Abs(minX) + 1, maxY + Int32.Abs(minY) + 1];
+            for(int x = 0; x < output.GetLength(0); x++)
+            {
+                for (int y = 0; y < output.GetLength(1); y++)
+                {
+                    output[x, y] = '.';
+                }
+            }
+
+            foreach(Coordinate position in positionsVisited)
+            {
+                output[position.X + Int32.Abs(minX), position.Y + Int32.Abs(minY)] = '#';
+            }
+
+            for (int y = output.GetLength(1) - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < output.GetLength(0); x++)
+                {
+                    Console.Write(output[x, y]);
+                }
+
+                Console.Write("\n");
+
+            }
+           
+        }
+
+        private static List<Coordinate> MoveRope(PuzzleInput puzzleInput, int countKnots)
         {
             List<Coordinate> visited = new List<Coordinate>();
-            Coordinate head = new Coordinate(0, 0);
-            Coordinate tail = new Coordinate(head);
+            List<Coordinate> knots = new List<Coordinate>();
+            for (int i = 1; i <= countKnots; i++)
+            {
+                knots.Add(new Coordinate(0, 0));
+            }
 
             foreach (string line in puzzleInput.Lines)
             {
                 char direction = line.Split(' ')[0][0];
                 int steps = Int32.Parse(line.Split(' ')[1]);
 
-                MoveHead(head, tail, direction, steps, visited);
+                MoveHead(knots, direction, steps, visited);
               
             }
 
@@ -79,8 +120,10 @@ namespace Day09
             tail.Move(xToAdd, yToAdd);
         }
 
-        private static void MoveHead(Coordinate head, Coordinate tail, char direction, int steps, List<Coordinate> visited)
+        private static void MoveHead(List<Coordinate> knots, char direction, int steps, List<Coordinate> visited)
         {
+            Coordinate head = knots[0];
+
             for (int s = 1; s <= steps; s++)
             {
                 if (direction == 'U')
@@ -100,11 +143,17 @@ namespace Day09
                     head.Move(1, 0);
                 }
 
-                MoveTail(head, tail);
-                if (!visited.Any(v => v.Equals(tail)))
+                for(int i = 1; i < knots.Count; i++)
                 {
-                    visited.Add(new Coordinate(tail));
+                    MoveTail(knots[i-1], knots[i]);
+                   
                 }
+
+                if (!visited.Any(v => v.Equals(knots.Last())))
+                {
+                    visited.Add(new Coordinate(knots.Last()));
+                }
+
             }
         }
     }
