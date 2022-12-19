14900caa1295c8a36716b731c8258718efa39154
joko-dev <jokodev@outlook.de>
2022-12-20 00:23:10 +0100

    Better adjacence check

--- AdventOfCode2022/Day18/Program.cs
+++ AdventOfCode2022/Day18/Program.cs
@@ -28,53 +28,10 @@ namespace Day18
             {
                 Coordinate = new Coordinate3D(x, y, z);
             }
-            internal List<List<Coordinate3D>> GetPlanes()
+            internal bool IsAdjacent(Cube other)
             {
-                List<List<Coordinate3D>> planes = new List<List<Coordinate3D>>();
-
-                List<Coordinate3D> plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z + 1));
-                planes.Add(plane);
-
-                plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z + 1));
-                planes.Add(plane);
-
-                plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z));
-                planes.Add(plane);
-
-                plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z));
-                planes.Add(plane);
-
-                plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z));
-                planes.Add(plane);
-
-                plane = new List<Coordinate3D>();
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X + 1, Coordinate.Y + 1, Coordinate.Z + 1));
-                plane.Add(new Coordinate3D(Coordinate.X, Coordinate.Y + 1, Coordinate.Z + 1));
-                planes.Add(plane);
-
-                return planes;
+                int diff = Math.Abs(this.Coordinate.X - other.Coordinate.X) + Math.Abs(this.Coordinate.Y - other.Coordinate.Y) + Math.Abs(this.Coordinate.Z - other.Coordinate.Z);
+                return diff == 1;
             }
         }
         static void Main(string[] args)
@@ -91,8 +48,6 @@ namespace Day18
 
         private static int GetSurfaceArea(List<Cube> cubes)
         {
-            //Idea: get all planes of a cube. Compare them to all other planes of all other cubes. If all points of one plane equals all points of another, 
-            //      the cubes are touching. 
             int surfaceArea = 0;
 
             foreach (Cube cube1 in cubes)
@@ -105,31 +60,16 @@ namespace Day18
 
         private static int GetSideNotConnected(Cube cube1, List<Cube> cubes)
         {
-            int sidesNotConnected = 0;
-            foreach (List<Coordinate3D> plane1 in cube1.GetPlanes())
+            int sidesConnected = 0;
+
+            foreach (Cube cube2 in cubes.Where(c => c != cube1))
             {
-                bool touching = false;
-                foreach (Cube cube2 in cubes.Where(c => c != cube1))
+                if (cube1.IsAdjacent(cube2))
                 {
-                    foreach (List<Coordinate3D> plane2 in cube2.GetPlanes())
-                    {
-                        if(ArePlanesEqual(plane1, plane2))
-                        {
-                            touching = true;
-                            break;
-                        }
-                    }
-                    if (touching)
-                    {
-                        break;
-                    }
-                }
-                if (!touching)
-                {
-                    sidesNotConnected++;
+                    sidesConnected++;
                 }
             }
-            return sidesNotConnected;
+            return (6- sidesConnected);
         }
 
         private static bool ArePlanesEqual(List<Coordinate3D> plane1, List<Coordinate3D> plane2)
