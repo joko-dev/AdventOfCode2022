b64275684539f92e1a61071cfa983d2b92795063
joko-dev <jokodev@outlook.de>
2022-12-24 13:21:00 +0100

    Day18 - Part 2

--- AdventOfCode2022/Day18/Program.cs
+++ AdventOfCode2022/Day18/Program.cs
@@ -33,6 +33,11 @@ namespace Day18
                 int diff = Math.Abs(this.Coordinate.X - other.Coordinate.X) + Math.Abs(this.Coordinate.Y - other.Coordinate.Y) + Math.Abs(this.Coordinate.Z - other.Coordinate.Z);
                 return diff == 1;
             }
+
+            internal bool Equals(Cube other)
+            {
+                return this.Coordinate.Equals(other.Coordinate);
+            }
         }
         static void Main(string[] args)
         {
@@ -42,17 +47,80 @@ namespace Day18
 
             //Visualization: https://www.matheretter.de/geoservant/en
             List<Cube> cubes = CreateCubes(puzzleInput.Lines);
-
             Console.WriteLine("Surface area: {0}", GetSurfaceArea(cubes));
+
+            // Part 2 - Flood fill. Get cubes outside of form, then check all frop cubes which are connected to the outside cubes
+            List<Cube> outside = GetOutsideCubes(cubes);
+            Console.WriteLine("Surface area: {0}", GetExteriorSurfaceArea(cubes, outside));
+        }
+
+        private static List<Cube> GetOutsideCubes(List<Cube> cubes)
+        {
+            List<Cube> outside = new List<Cube>();
+            Cube start = new Cube(0, 0, 0);
+            int maxX = cubes.Max(c => c.Coordinate.X) + 1;
+            int maxY = cubes.Max(c => c.Coordinate.Y) + 1;
+            int maxZ = cubes.Max(c => c.Coordinate.Z) + 1;
+
+            int minX = cubes.Min(c => c.Coordinate.X) - 1;
+            int minY = cubes.Min(c => c.Coordinate.Y) - 1;
+            int minZ = cubes.Min(c => c.Coordinate.Z) - 1;
+
+            floodFillOutside(outside, cubes, start, minX, minY, minZ, maxX, maxY, maxZ);
+
+            return outside;
+        }
+
+        private static void floodFillOutside(List<Cube> outside, List<Cube> inside, Cube start, int minX, int minY, int minZ, int maxX, int maxY, int maxZ)
+        {
+            // Recursive algorithm will lead to a stack overflow
+            Stack<Cube> stack = new Stack<Cube>();
+            stack.Push(start);
+            while (stack.Count > 0)
+            {
+                Cube toFill = stack.Pop();
+                if (IsValidCube(toFill, outside, inside, minX, minY, minZ, maxX, maxY, maxZ))
+                {
+                    outside.Add(toFill);
+                    stack.Push(new Cube(toFill.Coordinate.X + 1, toFill.Coordinate.Y, toFill.Coordinate.Z));
+                    stack.Push(new Cube(toFill.Coordinate.X - 1, toFill.Coordinate.Y, toFill.Coordinate.Z));
+                    stack.Push(new Cube(toFill.Coordinate.X, toFill.Coordinate.Y + 1, toFill.Coordinate.Z));
+                    stack.Push(new Cube(toFill.Coordinate.X, toFill.Coordinate.Y - 1, toFill.Coordinate.Z));
+                    stack.Push(new Cube(toFill.Coordinate.X, toFill.Coordinate.Y, toFill.Coordinate.Z + 1));
+                    stack.Push(new Cube(toFill.Coordinate.X, toFill.Coordinate.Y, toFill.Coordinate.Z - 1));
+                }
+            }
+        }
+
+        private static bool IsValidCube(Cube toFill, List<Cube> outside, List<Cube> inside, int minX, int minY, int minZ, int maxX, int maxY, int maxZ)
+        {
+            bool valid = toFill.Coordinate.X >= minX && toFill.Coordinate.X <= maxX
+                            && toFill.Coordinate.Y >= minY && toFill.Coordinate.Y <= maxY
+                            && toFill.Coordinate.Z >= minZ && toFill.Coordinate.Z <= maxZ
+                            && !outside.Exists(c => c.Equals(toFill))
+                            && !inside.Exists(c => c.Equals(toFill));
+            return valid;
+        }
+
+        private static int GetExteriorSurfaceArea(List<Cube> cubes, List<Cube> outside)
+        {
+            int surfaceArea = 0;
+
+            foreach (Cube cube in cubes)
+            {
+                surfaceArea += GetSidesConnected(cube, outside);
+            }
+
+            return surfaceArea;
         }
 
         private static int GetSurfaceArea(List<Cube> cubes)
         {
             int surfaceArea = 0;
 
-            foreach (Cube cube1 in cubes)
+            foreach (Cube cube in cubes)
             {
-                surfaceArea += GetSideNotConnected(cube1, cubes);
+                surfaceArea += GetSideNotConnected(cube, cubes);
             }
 
             return surfaceArea;
@@ -69,20 +137,21 @@ namespace Day18
                     sidesConnected++;
                 }
             }
-            return (6- sidesConnected);
+            return (6 - sidesConnected);
         }
 
-        private static bool ArePlanesEqual(List<Coordinate3D> plane1, List<Coordinate3D> plane2)
+        private static int GetSidesConnected(Cube cube1, List<Cube> cubes)
         {
-            foreach(Coordinate3D point1 in plane1)
+            int sidesConnected = 0;
+
+            foreach (Cube cube2 in cubes)
             {
-                bool found = plane2.Exists(c => c.Equals(point1));
-                if (!found)
+                if (cube1.IsAdjacent(cube2))
                 {
-                    return false;
+                    sidesConnected++;
                 }
             }
-            return true;
+            return sidesConnected;
         }
 
         private static List<Cube> CreateCubes(List<string> lines)
